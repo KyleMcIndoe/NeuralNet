a4635335c75fe88e9895511f87df5c623e9b63a6
Kyle McIndoe <kmcindoe06@gmail.com>
2024-09-04 10:50:21 -0400

    Add project files.

---
+++ Form1.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace NeuralNet
+{
+    public class network {
+        layer[] layers;
+        public network(int[] layersizes) {
+
+        }
+    }
+    public class layer {
+        double[,] weights;
+        double[] biases;
+        public layer(int nodecount)
+        {
+
+        }
+
+        
+    }
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+    }
+}
